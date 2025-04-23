629cc0d75581040a5bd54bff27edc4ffa4ae0364
SavitarN <nischalzoc999@gmail.com>
2025-04-23 16:59:56 +0545

    contact us form validation

--- src/component/ContactForm.jsx
+++ src/component/ContactForm.jsx
@@ -13,39 +13,73 @@ const ContactForm = () => {
       onSubmit={handleSubmit((data) => {
         console.log(data);
       })}
-      className="p-5 border border-blue-200/10 text-white flex flex-col items-center gap-4 w-[50%]"
+      className="p-5 border border-blue-400/10 rounded text-white flex flex-col items-center gap-4 w-[50%]"
     >
       <input
         {...register("firstName", {
           required: { value: true, message: "This field cannot be empty" },
           maxLength: {
             value: 20,
-            message: "Maximum character you can have is 20",
+            message: "Maximum character length can be 20",
+          },
+          minLength: {
+            value: 3,
+            message: "Minimum chracter length should be 3",
+          },
+          pattern: {
+            value: /^[A-Za-z\s]+$/,
+            message: "Name should only contain letters and spaces",
           },
         })}
         type="text"
-        className="bg-black text-white w-[70%] rounded p-2"
+        className="bg-black text-white w-[70%] rounded p-2 max-sm:px-4  max-sm:w-[100%]"
         placeholder="Username"
       />
-      {errors.firstName && <span>{errors.firstName.message}</span>}
+
+      <p className="min-h-[1em]">
+        {errors.firstName && (
+          <span className="text-red-400 font-semibold">
+            {errors.firstName.message}
+          </span>
+        )}
+      </p>
+
       <input
         {...register("email", {
           required: { value: true, message: "This field cannot be empty" },
+          pattern: {
+            value: /^[^\s@]+@[^\s@]+\.[^\s@]+$/,
+            message: "Enter a valid email address",
+          },
         })}
         type="text"
-        className="bg-black text-white w-[70%] rounded p-2"
+        className="bg-black text-white w-[70%] rounded p-2 max-sm:w-[100%]  max-sm:px-4 "
         placeholder="youremali@gmail.com"
       />
-      {errors.email && <span>{errors.email.message}</span>}
+      <p className="min-h-[1em]">
+        {errors.email && (
+          <span className="text-red-400 font-semibold">
+            {errors.email.message}
+          </span>
+        )}
+      </p>
+
       <textarea
         {...register("message", {
           required: { value: true, message: "This field cannot be empty" },
         })}
         rows={10}
-        className="bg-black text-white w-[70%] rounded p-2"
+        className="bg-black text-white w-[70%] rounded p-2  max-sm:w-[100%]  max-sm:px-4 "
         placeholder="Any message for us ?"
       />
-      {errors.message && <span>{errors.message.message}</span>}
+      <p className="min-h[1em]">
+        {errors.message && (
+          <span className="text-red-400 font-semibold">
+            {errors.message.message}
+          </span>
+        )}
+      </p>
+
       <button
         className="w-fit  btn-navy btn-navy:hover px-5 py-1 rounded"
         type="submit"
--- src/section/Contact.jsx
+++ src/section/Contact.jsx
@@ -3,7 +3,7 @@ import ContactForm from "../component/ContactForm";
 
 const Contact = () => {
   return (
-    <section className="w-full m-w-[1400px] bg-midnight flex items-center  flex-col p-10 text-white">
+    <section className="w-full m-w-[1400px] bg-midnight flex items-center  flex-col p-10 text-white ">
       <p className="text-slate-400 font-bold mb-5">
         We would love to hear from you{" "}
       </p>
